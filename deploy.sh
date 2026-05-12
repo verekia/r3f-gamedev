8ba4241daa144e0451535a3a4d466f4a8ab8ebbf
verekia <522007+verekia@users.noreply.github.com>
2026-05-12 15:36:11 +0700

    perf: gzip image

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker buildx build --platform linux/arm64 --load -t verekia/r3f-gamedev .
-docker save -o /tmp/r3f-gamedev.tar verekia/r3f-gamedev
-scp /tmp/r3f-gamedev.tar midgar:/tmp/
-ssh midgar docker load --input /tmp/r3f-gamedev.tar
+docker save verekia/r3f-gamedev | gzip > /tmp/r3f-gamedev.tar.gz
+scp /tmp/r3f-gamedev.tar.gz midgar:/tmp/
+ssh midgar docker load --input /tmp/r3f-gamedev.tar.gz
 ssh midgar docker compose up -d r3f-gamedev
